6bf759e35290820747033204ec4f59bade3e4de6
Bryon Vandiver <unicdk@gmail.com>
2020-10-26 12:58:01 -0700

    Checkpoint

--- src/c88as.cli.js
+++ src/c88as.cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const yargs = require('yargs/yargs');
 
-const assembler = require('./as')
+const argv = yargs(process.argv.slice(2))
+    .usage('Usage: $0 <command> [options]')    
+    .help('h')
+    //.boolean(['r','v'])
+    .argv
+    ;
+
+console.dir(argv);
