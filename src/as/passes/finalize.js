278abffdfccb65fd4a89746d3c7c4c10760b23e1
Bryon Vandiver <unicdk@gmail.com>
2021-08-06 18:00:40 -0700

    Woopsie

--- src/as/passes/finalize.js
+++ src/as/passes/finalize.js
@@ -7,46 +7,6 @@ const { LEVEL_FATAL, LEVEL_FAIL, LEVEL_WARN, LEVEL_INFO, Message } = require (".
 
 const encoder = new TextEncoder();
 
-function* TypedDataBlock({ type, data }, cast, array) {
-    let start = 0;
-
-    while (start < data.length) {
-        // Find ranges
-        let range_start = start;
-        while (range_start < data.length && !isValueType(data[range_start])) range_start++;
-        let range_end = range_start;
-        while (range_end < data.length && isValueType(data[range_end])) range_end++;
-
-        // Cast off unevaluated stuff
-        if (start != range_start) {
-            const slice = data.slice(start, range_start);
-
-            yield {
-                type,
-                data: slice,
-                location: slice[0].location
-            };
-        }
-
-        // Emit typed arrays for remaining data
-        const slice = data.slice(range_start, range_end).map(cast);
-        if (array) {
-            const arr = new array(slice);
-            for (let i = 0; i < slice.length; i++) {
-                if (arr[i] != slice[i]) {
-                    yield new Message(LEVEL_WARN, data[i+range_start].location, `Value ${slice[i]} truncated to ${arr[i]}`);
-                }
-            }
-
-            yield arr;
-        } else {
-            yield* slice;
-        }
-
-        start = range_end;
-    }
-}
-
 async function* finalize(scope, tree) {
     for await (let token of tree) {
         if (token instanceof Message) {
