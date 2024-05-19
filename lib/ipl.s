f70cc2dd8030566926cc830d40cf06e9be4d9835
Bryon Vandiver <unicdk@gmail.com>
2024-05-18 21:00:02 -0700

    Proper names for boot

--- lib/ipl.s
+++ lib/ipl.s
@@ -3,22 +3,22 @@
 ; ===
 irq DUPA flag, 277h, 276h, 275h, 274h, 273h, 272h, 271h, 270h, 285h, 284h, 283h, 282h, 2A7h, 2A6h, 297h, 296h, 295h, 294h, 293h, 292h, 291h, 290h, 2A2h, 2A1h, 2A0h, 280h
         DEFSECT (".irq_weak_handler" .. irq), CODE, FIT 8000H
-        SECT ".irq_weak_handler"
+        SECT (".irq_weak_handler" .. irq)
+        GLOBAL WEAK _interrupt_handler\?irq
 _interrupt_handler\?irq:
         PUSH BR
         LD BR, #20h
         LD [BR:(flag >> 4)], #(1 << (flag & 0fh))
         POP BR
         RETE
-        GLOBAL WEAK _interrupt_handler\?irq
 ENDM
 
 ; ===
 ; === Memory scatter loader
 ; ===
 
-        DEFSECT ".init_ram", CODE, FIT 8000H, ROMDATA
-        SECT ".init_ram"
+        DEFSECT ".scatter_load", CODE, FIT 8000H, ROMDATA
+        SECT ".scatter_load"
 _scatter_load:
         ; This will initialize ram
         JRL _start
